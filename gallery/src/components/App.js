c58472ef3e13bbe9b0366485270735f2441b7f60
Daniil Sergeev <daniil.scorpio@gmail.com>
2023-11-02 21:35:53 +0300

    refactor: create react app again

--- gallery/src/components/App.js
+++ gallery/src/components/App.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import Header from './Header';
-import SearchContainer from './SearchContainer';
-import Gallery from './Gallery';
-import Modal from './Modal';
-import Footer from './Footer';
 
 import '../styles/App.css';
+import React from 'react';
+import Header from './Header.js';
+import SearchContainer from './SearchContainer.js';
+import Gallery from './Gallery.js';
+import Modal from './Modal.js';
+import Footer from './Footer.js';
 
 class App extends React.Component {
   render() {
