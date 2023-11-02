861b20c1d51f10770a1c31490d19eddcb8d3fb58
Daniil Sergeev <daniil.scorpio@gmail.com>
2023-11-02 19:17:36 +0300

    feat:create header component

---
+++ gallery/src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import '../styles/Header.css';
+
+class Header extends React.Component {
+    render() {
+        return <header className='Header'>
+            <h1>Галерея изображений</h1>
+        </header>
+    }
+}
+
+export default Header;
