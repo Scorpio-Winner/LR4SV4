1fa4ca909813d0169bf15b668f60dfc857e99f65
Daniil Sergeev <daniil.scorpio@gmail.com>
2023-11-06 16:23:33 +0300

    feat: add header

---
+++ gallery1.2/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import '../styles/Header.css';
+
+class Header extends React.Component {
+    render() {
+        return <header className='header'>
+            <h1>Image gallery</h1>
+        </header>
+    }
+}
+
+export default Header;
