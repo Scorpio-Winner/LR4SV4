ca78430ea9f61cadc9df3804654b79969669441b
Daniil Sergeev <daniil.scorpio@gmail.com>
2023-11-06 16:31:25 +0300

    feat: update app code

--- gallery1.2/src/components/App.jsx
+++ gallery1.2/src/components/App.jsx
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Header from './Header';
+import SearchBar from './SearchBar';
+import Gallery from './Gallery';
+import Footer from './Footer';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import '../styles/App.css';
+
+class App extends React.Component {
+    constructor() {
+        super();
+        this.state = { images: [] }
+        this.updateGallery = this.updateGallery.bind(this);
+    }
+
+    updateGallery(updatedImages) {
+        this.setState({ images: updatedImages });
+    }
+
+    render() {
+        return (
+            <>
+                <Header />
+                <main>
+                    <SearchBar updateGallery={this.updateGallery} />
+                    <Gallery images={this.state.images} />
+                </main>
+                <Footer />
+            </>
+        )
+    }
 }
 
 export default App;
